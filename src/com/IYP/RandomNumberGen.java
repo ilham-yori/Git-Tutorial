7be1319321bc305e3922cffe8cbb9cefcbe29acd
Ilham Yori Pradana <ilhamyp@student.ittelkom-sby.ac.id>
2021-11-01 15:06:33 +0700

    Add Overload Function

--- src/com/IYP/RandomNumberGen.java
+++ src/com/IYP/RandomNumberGen.java
@@ -7,4 +7,8 @@ public class RandomNumberGen {
     public int getRandomNumber(){
         return new Random().nextInt();
     }
+
+    public int getRandomNumber(int limit){
+        return new Random().nextInt(limit);
+    }
 }
