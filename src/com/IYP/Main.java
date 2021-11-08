16f07dd283ec92583b39c26897623fbb5ceed8f5
Ilham Yori Pradana <ilhamyp@student.ittelkom-sby.ac.id>
2021-11-08 13:56:21 +0700

    Change GetRandomGen to Static

--- src/com/IYP/Main.java
+++ src/com/IYP/Main.java
@@ -5,6 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Halo, Dunia!" + new RandomNumberGen().getRandomNumber());
+        System.out.println("Halo, Dunia!" + RandomNumberGen.getRandomNumber());
     }
 }
